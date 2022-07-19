1fa70274caede9c0a9c29e101c451d39e00c0929
jhonny stiven agudelo tenorio <jhonny.agudelo3120@gmail.com>
2022-07-19 11:40:09 -0500

    navbar journal

--- React-cero-experto/JournalApp/src/journal/layout/JournalLayout.jsx
+++ React-cero-experto/JournalApp/src/journal/layout/JournalLayout.jsx
@@ -1,24 +1,20 @@
-import { Box } from '@mui/system'
-import { React } from 'react'
-
+import { Box } from "@mui/system";
+import { React } from "react";
+import { NavBar, SideBar } from "../../ua/components";
+const drawerWidth = 240;
 const JournalLayout = ({ children }) => {
   return (
     <>
-      <Box
-        sx={{ display: 'flex' }}
-      >
-        {/* NavBar */}
-        {/* Sidebar */}
-        <Box component='main'
-          sx={{ flexGrow: 1, p: 3 }}
-        >
+      <Box sx={{ display: "flex" }}>
+        <NavBar drawerWidth={drawerWidth} />
+        <SideBar drawerWidth={drawerWidth} />
+        <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
           {/* toolbar */}
           {children}
-
         </Box>
       </Box>
     </>
-  )
-}
+  );
+};
 
-export { JournalLayout }
+export { JournalLayout };
